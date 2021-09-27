ddbc55e8619c9a8af991aea1e90dc4168235a538
OverLore <44379883+OverLore@users.noreply.github.com>
2021-09-27 17:58:25 +0200

    Ajout des balles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] GameObject bullet;
+
+    public float shotForce;
+
+    float nextShot;
+    float delay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextShot = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        nextShot -= Time.deltaTime;
+
+        if (nextShot < 0)
+        {
+            nextShot = delay;
+
+            GameObject go = Instantiate(bullet);
+
+            go.transform.position = transform.position;
+
+            go.GetComponent<Rigidbody2D>().velocity = Vector3.up * shotForce;
+        }
     }
 }
