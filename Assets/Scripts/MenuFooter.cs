551ca49de0a7e23d489204fd9f3723fbe48f6728
OverLore <44379883+OverLore@users.noreply.github.com>
2021-09-29 16:56:27 +0200

    Navbar menu main fonctionnelle

--- Assets/Scripts/MenuFooter.cs
+++ Assets/Scripts/MenuFooter.cs
@@ -4,21 +4,68 @@ using UnityEngine.UI;
 
 public class MenuFooter : MonoBehaviour
 {
-    [SerializeField] List<Image> ButtonsList = new List<Image>();
-    [SerializeField] Canvas MainCanvas;
-
-    void Update()
+    public enum MenuTab
     {
-        float width = MainCanvas.GetComponent<RectTransform>().rect.width;
-        float height = MainCanvas.GetComponent<RectTransform>().rect.height;
-        float imgsScale = width / ButtonsList.Count;
+        Shop,
+        Achievements,
+        Home,
+        Skins,
+        Settings
+    }
 
-        for (int i = 0; i < ButtonsList.Count; i++)
+    float DefaultSize;
+    float SelectedSize;
+
+    [Header("Settings")]
+    [SerializeField] Canvas MainCanvas;
+    [SerializeField] Color NormalColor;
+    [SerializeField] Color SelectColor;
+    [SerializeField] MenuTab SelectedTab;
+    [SerializeField] List<Image> ButtonsList = new List<Image>();
+
+    Dictionary<MenuTab, Image> Tabs = new Dictionary<MenuTab, Image>();
+
+    private void Awake()
+    {
+        Tabs.Add(MenuTab.Shop, ButtonsList[0]);
+        Tabs.Add(MenuTab.Achievements, ButtonsList[1]);
+        Tabs.Add(MenuTab.Home, ButtonsList[2]);
+        Tabs.Add(MenuTab.Skins, ButtonsList[3]);
+        Tabs.Add(MenuTab.Settings, ButtonsList[4]);
+
+        DefaultSize = MainCanvas.GetComponent<RectTransform>().rect.width / (ButtonsList.Count + 1);
+        SelectedSize = DefaultSize * 2;
+
+        foreach (Image img in ButtonsList)
         {
-            Image img = ButtonsList[i];
-
-            img.rectTransform.sizeDelta = new Vector2(imgsScale, imgsScale);
-            img.transform.localPosition = new Vector3((-width / 2) + imgsScale / 2 + (i * imgsScale), -height / 2 + imgsScale / 2, 0);
+            img.color = NormalColor;
         }
+
+        OpenTab(SelectedTab);
+    }
+
+    private void Update()
+    {
+        DefaultSize = MainCanvas.GetComponent<RectTransform>().rect.width / (ButtonsList.Count + 1);
+        SelectedSize = DefaultSize * 2;
+
+        OpenTab(SelectedTab);
+    }
+
+    public void OpenTab(MenuTab tab)
+    {
+        Tabs[SelectedTab].rectTransform.sizeDelta = new Vector2(DefaultSize, DefaultSize);
+        Tabs[SelectedTab].color = NormalColor;
+
+
+        SelectedTab = tab;
+
+        Tabs[SelectedTab].rectTransform.sizeDelta = new Vector2(SelectedSize, DefaultSize);
+        Tabs[SelectedTab].color = SelectColor;
+    }
+
+    public void OpenTab(int tabID)
+    {
+        OpenTab((MenuTab)tabID);
     }
 }
