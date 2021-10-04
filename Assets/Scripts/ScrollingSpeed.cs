b9bdbcd92bb4764aa6c3bcae979a720714cdf450
Ebatsik <ebatsik@gmail.com>
2021-10-04 10:49:54 +0200

    add scrolling speed start

---
+++ Assets/Scripts/ScrollingSpeed.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingSpeed : MonoBehaviour
+{
+    //depth layer will determine the scrolling speed in an idea of parallax implementation
+    [SerializeField] int depthLayer;
+    int maxDepthLayer = 10;
+
+    float DetermineScrollingSpeed()
+    {
+        float result = 0;
+
+        return result;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
