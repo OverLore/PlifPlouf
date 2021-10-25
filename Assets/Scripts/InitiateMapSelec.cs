3feebaabe8a67379d842f21a3b3db6f1aa3a56bc
Lagardere <mathis.lagardere.pro@gmail.com>
2021-10-25 17:08:12 +0200

    Map level select

--- Assets/Scripts/InitiateMapSelec.cs
+++ Assets/Scripts/InitiateMapSelec.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SplashScreen : MonoBehaviour
+public class InitiateMapSelec : MonoBehaviour
 {
+    [SerializeField]  GameObject[] tabButtons;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int x = 0; x < GameManager.instance.maxLevelReached; )
+        {
+            tabButtons[x].SetActive(true);
+        }
     }
 
     // Update is called once per frame
