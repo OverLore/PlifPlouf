75e57226d1e454c5bb82496b3a24ae17463a2266
Ebatsik <ebatsik@gmail.com>
2021-11-28 19:56:42 +0100

    scale tout tel appliqué au menu
    si ca casse un truc pour vous, desactiver le gameObject "CanvasScreenScaler" (mais normalement si vous etes sur la resolution du note 7 il devrait y avoir aucun changement

--- Assets/Scripts/UI/Menu/CanvasScreenScaler.cs
+++ Assets/Scripts/UI/Menu/CanvasScreenScaler.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PreMenuManager : MonoBehaviour
+public class CanvasScreenScaler : MonoBehaviour
 {
-    [SerializeField] GameObject preMenuCanvas;
+    [SerializeField] GameObject[] canvasArray;
 
     // Start is called before the first frame update
     void Start()
     {
         CameraManager cm = Camera.main.GetComponent<CameraManager>();
-        cm.SetCanvasScaleToCameraSize(preMenuCanvas);
+        foreach (GameObject canvas in canvasArray)
+        {
+            cm.SetCanvasScaleToCameraSize(canvas);
+        }
     }
 }
