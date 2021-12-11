a92e832226d1a6db6db2a86d4292d688b9cd79f6
Ebatsik <ebatsik@gmail.com>
2021-12-11 17:34:30 +0100

    fix not enough money anim restait quand on changeait de tab et on revenait

--- Assets/Scripts/UI/Menu/UpgradeOnClick.cs
+++ Assets/Scripts/UI/Menu/UpgradeOnClick.cs
@@ -25,8 +25,18 @@ public class UpgradeOnClick : MonoBehaviour
         
     }
 
+    public void CloseBox()
+    {
+        //Debug.Log("skins tab");
+        //gameObject.SetActive(true);
+        //GetComponent<Animator>().SetTrigger("IsClosing");
+        //DisableIsShown();
+        //gameObject.SetActive(false);
+    }
+
     public void DisableIsShown()
     {
+        Debug.Log("Disable is shown");
         transform.SetAsLastSibling();
         GetComponent<Animator>().SetBool("IsShown", false);
     }
